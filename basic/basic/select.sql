fd2b1d85c00657479843cb293eada2ff6571b67c
AsmaaSobhy950 <156791183+AsmaaSobhy950@users.noreply.github.com>
2026-03-07 02:28:42 +0200

    Add various SQL queries for employee data retrieval

--- basic/basic/select.sql
+++ basic/basic/select.sql
@@ -2,4 +2,86 @@
 select* 
 from employees
 
+--Show all columns for any 10 rows from employees
+SELECT *
+FROM employees
+LIMIT 10;
+
+-- Show only emp_no, first_name, last_name from employees
+SELECT emp_no, first_name, last_name
+FROM employees;
+
+-- From departments, list dept_no and dept_name
+SELECT dept_no, dept_name
+FROM departments;
+
+-- From salaries, show emp_no, salary, from_date, to_date for any 5 rows
+SELECT emp_no, salary, from_date, to_date
+FROM salaries
+LIMIT 5;
+
+-- List the distinct title values from titles
+SELECT DISTINCT title
+FROM titles;
+
+--List distinct gender values from employees
+SELECT DISTINCT gender
+FROM employees;
+
+--List distinct department names (dept_name) from departments
+SELECT DISTINCT dept_name
+FROM departments;
+---sql where---
+--Employees whose first_name = 'Georgi'
+SELECT *
+FROM employees
+WHERE first_name = 'Georgi';
+
+--Employees hired after 1999‑01‑01
+SELECT *
+FROM employees
+WHERE hire_date > '1999-01-01';
+
+--Salaries greater than 120000
+SELECT *
+FROM salaries
+WHERE salary > '120000';
+
+--Titles where the title contains 'Engineer'
+SELECT *
+FROM titles
+WHERE title LIKE '%Engineer%';
+
+--Employees with birth_date between 1960‑01‑01 and 1965‑12‑31
+SELECT *
+FROM employees
+WHERE birth_date BETWEEN '1960-01-01' AND '1965-12-31';
+
+--------SQL ORDER BY--------
+--List employees (emp_no, first_name, last_name) ordered by hire_date ASC
+SELECT emp_no, first_name, last_name
+FROM employees
+ORDER BY hire_date ASC;
+
+--List the 20 most recent hires ordered by hire_date DESC
+SELECT emp_no, CONCAT(first_name, ' ', last_name) AS name, hire_date
+FROM employees
+ORDER BY hire_date DESC
+LIMIT 20;
+
+--List departments ordered alphabetically by dept_name
+SELECT dept_no, dept_name
+FROM departments
+ORDER BY dept_name ;
+
+--Employees where gender = 'F' AND hire_date ≥ '2000-01-01'
+SELECT *
+FROM employees
+WHERE gender = 'F'
+  AND hire_date >= '2000-01-01';
+--From salaries: salary ≥ 100000 AND to_date = '9999-01-01'
+SELECT *
+FROM salaries
+WHERE salary >= 100000
+  AND to_date = '9999-01-01';
 
